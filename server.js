ff1cf24d1d1b28e18ae6160fb47b4bd648bb6f39
Edgar Munoz <edgar.munoz@latam.com>
2017-06-03 13:03:07 -0400

    add express to project

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+	res.send('Hola mundo!')
+})
+
+app.listen(3000, function(err){
+	if(err){
+		return console.log("Existió un error"), process.exit(1);
+	}
+	console.log("Platzigram escuchando en el puerto 3000");
+})
