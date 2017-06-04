b3d46d9f6637ab99ad92a9ee20faf5da50139697
Edgar Munoz <edgar.munoz@latam.com>
2017-06-04 18:41:08 -0400

    format for translate text

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -14,7 +14,7 @@ page('/', function (ctx, next) {
         avatar: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767'
       },
       url: 'office.jpg',
-      likes: 10,
+      likes: 0,
       liked: false,
       createdAt: new Date().getTime()
     },
@@ -24,7 +24,7 @@ page('/', function (ctx, next) {
         avatar: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767'
       },
       url: 'office.jpg',
-      likes: 2,
+      likes: 1,
       liked: true,
       createdAt: new Date().setDate(new Date().getDate() - 10)
     }
