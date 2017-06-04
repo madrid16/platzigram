104b2721088778e758912b8280306757fdf26b32
Edgar Munoz <edgar.munoz@latam.com>
2017-06-04 18:01:53 -0400

    add timeline to users

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -6,5 +6,27 @@ var title = require('title');
 page('/', function (ctx, next) {
   title('Platzigram');
   var main = document.getElementById('main-container');
-  empty(main).appendChild(template);
+
+  var pictures = [
+    {
+      user: {
+        username: 'slifszyc',
+        avatar: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767'
+      },
+      url: 'office.jpg',
+      likes: 10,
+      liked: true
+    },
+    {
+      user: {
+        username: 'slifszyc',
+        avatar: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767'
+      },
+      url: 'office.jpg',
+      likes: 2,
+      liked: true
+    }
+  ];
+
+  empty(main).appendChild(template(pictures));
 })
